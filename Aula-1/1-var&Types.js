ed23bb11928b784a8bf0f84960dd95c1d3f7c99c
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-03-22 15:52:03 -0300

    push to master

---
+++ Aula-1/1-var&Types.js
@@ -0,0 +1,25 @@
+/*
+  Uma var se declaracao de valor, seu valor é Undefined.
+    ex.: var x 
+      console.logo(x) x undefined
+    
+    x = 0
+    o x, vai ter uma nova atribuicao, no caso o valor dele sera 0.
+
+    x= -1
+    X agora tera novamente um novo valor, que sera -1
+
+
+
+
+    <-- Tipo de Dados -->
+
+    String, number, boolean(false or true)*1, null, undefined, object*2, arr*3.
+
+    *1, lembrando que o js tem uma inteligencia de falsy, e o trutly
+    *2, mais importante. Geralmente uma Apicall retornar um dado do tipo objeto.
+    *3, Arr e o Obj sao praticamente a msm coisa dentro do Js, mudar somente a sintaxe que eles iram receber, mas a forma de ser acessado sera a mesma.
+    
+
+    
+*/
---
+++ Aula-1/2-OpArt.js
@@ -0,0 +1,35 @@
+/*
+  <-- Expressao no Js-->
+    Sao frases em JS que sao avaliadas para produzirem certos valores.
+
+  <-- Op Aritmeticos -->
+  *,/,+,-
+  1 + 2 = 3 -> dis para o JS que ele vai pegar os valores e realizar a operacao passada.
+  2 - 1 = 1
+  ...
+  
+  Abeviados
+  += -> soma = soma + 20, pegar o valor da variavel, e acrescentar a operacao e o seu proprio valor
+    soma vale 40
+  -= ->
+    soma vale 20
+  *= ->
+    400
+  /= ->
+    1
+
+  
+
+  var soma = 10 + 10
+  soma vai valer 20 
+    soma = soma + 1 // 21
+      ||
+    soma ++ //21 , *1, lembra q ele incrementa automaticamente o valor 1. Igual em um looping.
+
+    *1, lembra que é um efeito cascata, pilha. Ele vai chamar a variavel soma primeiro depois incrementar ou realizar a op.
+      nesse caso, quando soma aparecer no console, sera com o valor ainda, vc precisara chamala novamente para apareer o valor atualizado
+      ou
+      ++soma, que ele incremente e ja atualiza o valor.
+
+
+*/
