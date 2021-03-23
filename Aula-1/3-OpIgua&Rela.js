3b7207c24acda5b67507ab1cf9b2d0a810f27b11
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-03-23 17:45:36 -0300

    update

---
+++ Aula-1/3-OpIgua&Rela.js
@@ -0,0 +1,19 @@
+/*
+  <-- Operadores de Igualdade -->
+  Verificar a igualdade dentre valores,*1
+  1 == 1 / true
+  1 == 1 / false
+  1 != 2 / true
+  2 != 2 / false
+
+  *1, estritamente igual que vai verificar nao somente o valor mas tbm o tipo da variavel, string.. number..
+
+  <-- Operadores de comparacao -->
+  vai comparar os valores
+  1 > 1 /false
+  1 >= 1 /true
+  1 > 2 /false
+  
+
+
+*/
